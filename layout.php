d731faf0f72466a76e608fba3160f05be6f13883
ATOMMIC <122683207+Arthittaya2508@users.noreply.github.com>
2023-10-27 20:54:15 +0700

    Add files via upload

---
+++ layout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML> <html>
+     <head>
+        <title>HTML5 header tag</title>
+        <meta charset=“utf-8”>
+        <?php include 'sheetcss.php' ?>
+</head>
+<body>
+<header>
+<h3>H3 ลำดับที่ 1</h3>
+<p>เนื้อหาใน H3 ลำดับที่ 1</p>
+<h4>H4 ลำดับที่ 1</h4>
+<p>เนื้อหาใน H4 ลำดับที่ 1</p>
+<h4>H4 ลำดับที่ 2</h4>
+<p>เนื้อหาใน H4 ลำดับที่ 2</p>
+<h3>H3 ลำดับที่ 2</h3>
+<p>เนื้อหาใน H3 ลำดับที่ 2</p>
+<h4>H4 ลำดับที่ 3</h4>
+<p>เนื้อหาใน H4 ลำดับที่ 3</p>
+<h4>H4 ลำดับที่ 4</h4>
+<p>เนื้อหาใน H4 ลำดับที่ 4</p>
+</header>
+</body>
+<?php include 'sheetjs.php' ?>
+</html>
